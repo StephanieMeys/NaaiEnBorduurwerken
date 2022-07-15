a6db000fc61b97f6028ab4db13433a9ba1dcf95f
StephDev <m.stephanie1001@gmail.com>
2022-07-15 20:57:49 +0200

    Progress

---
+++ main.js
@@ -0,0 +1,10 @@
+// Dropdown menu bij clicking the hamburg
+function menuOnClick() {
+    document.getElementById("hamburg").classList.toggle("change");
+    document.getElementById("nav").classList.toggle("change");
+}
+
+// Dropdown extra tools bij clicking the arrow
+function dropdownOnClick() {
+    document.getElementById("openDrop").classList.toggle("change");
+}
