864d6c76674e89c50bfbbd53cff60ce85b913955
StephDev <m.stephanie1001@gmail.com>
2022-08-21 13:37:20 +0200

    test

--- main.js
+++ main.js
@@ -8,3 +8,38 @@ function menuOnClick() {
 function dropdownOnClick() {
     document.getElementById("openDrop").classList.toggle("change");
 }
+
+// Carousel
+document.querySelectorAll(".carousel").forEach(carousel => {
+    const items = carousel.querySelectorAll(".carousel_item");
+    const dottHtml = Array.from(items, () => {
+        return '<span class="carousel-dott"></span>';
+    });
+
+    carousel.insertAdjacentHTML(
+        "beforeend",
+    `
+		<div class="carousel_nav">
+			${dottHtml.join("")}
+		</div>
+	`
+    );
+
+    const dotts = carousel.querySelectorAll("carousel-dott");
+
+    dotts.forEach((dott, i) => {
+        dott.addEventListener("click", () => {
+
+            items.forEach((item) => 
+            item.classList.remove("carousel-item-selected")
+            );
+            dotts.forEach((dott) => 
+            dott.classList.remove("carousel-dott-selected"));
+
+            items[i].classList.add("carousel-item--selected");
+            dott.classList.add("carousel-dott--selected")
+        });
+    });
+    items[0].classList.add("carousel-item--selected");
+  dotts[0].classList.add("carousel-button--selected");
+});
